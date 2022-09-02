ba989cd7b1abd7e9e4d12a29c2a218f2a4aa4ec4
Viktor Belous <vyntyk@yandex.ru>
2022-09-02 21:37:01 +0300

    Задача 66

--- Papka2/Program.cs
+++ Papka2/Program.cs
@@ -3,7 +3,6 @@
 M = 1; N = 15 -> 56
 M = 4; N = 8. -> 18
 */
-
 Console.Clear();
 Console.WriteLine("Задайте первое число: ");
 int m = int.Parse(Console.ReadLine()!);
@@ -11,6 +10,24 @@ int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Задайте второе число: ");
 int n = int.Parse(Console.ReadLine()!);
 
+int SumNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        if (m % 2 == 0)
+        {
+            return m + SumNumbers(m + 2, n);
+        }
+        else
+        {
+            m = m + 1;
+            return m + SumNumbers(m + 2, n);
+        }
+    }
+    else return 0;
+}
+
+Console.WriteLine(SumNumbers(m, n));
 
 int SumRec(int m, int n)
 {
@@ -20,5 +37,4 @@ int SumRec(int m, int n)
         if (i % 2 == 0) sum = sum + i;
     return sum;
 }
-
 Console.WriteLine(SumRec(m, n));
